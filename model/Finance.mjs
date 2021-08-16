b8806249e9cfffab50fc034369e366ee52df71df
Dmytro-Kanunik <Dmytro.Kanunik@coxautoinc.com>
2021-08-16 20:53:12 +0300

    add tests

---
+++ model/Finance.mjs
@@ -0,0 +1,32 @@
+import AppConfig from '../configs/AppConfig.cjs';
+
+import axios from "axios";
+
+class Finance {
+
+    //TODO: this is the bad method that should be replaced by getFinanceData method
+    static async getFinanceDataForAAPL() {
+        const params = 'chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance'
+        try {
+            return await axios.get(AppConfig.baseUrl + params);
+        } catch (e) {
+            console.log(`Request [${AppConfig.baseUrl}${params}] returned: `);
+            console.log(e);
+        }
+    }
+
+    // TODO: define parameters to be able to get dynamic test data
+    static async getFinanceData() {
+
+        // TODO: the value should contain template variables
+        const params = 'chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance'
+        try {
+            return await axios.get(AppConfig.baseUrl + params);
+        } catch (e) {
+            console.log(`Request [${AppConfig.baseUrl}${params}] returned: `);
+            console.log(e);
+        }
+    }
+}
+
+export default Finance;
--- tests/finance.test.mjs
+++ tests/finance.test.mjs
@@ -1,11 +1,50 @@
-import Finance from '../domain/Finance.mjs';
+import Finance from '../model/Finance.mjs';
 
 describe('As a Finance API user', () => {
-    describe('I need to get HTTP response code', () => {
-        test('200 for 1d range', async () => {
-            let { status } = await Finance.getFinanceData();
+    describe('I have to get HTTP response code', () => {
+        xtest('200 for a valid range', async () => {
+
+            //TODO: replace this method with Finance.getFinanceData(...) method
+            // to be able to define different test conditions:
+            // - company
+            // - region
+            // - interval
+            // - range
+            let { status } = await Finance.getFinanceDataForAAPL();
             expect(status).toBe(200);
+        });
+
+        xtest('422 for a invalid range', async () => {
+            // TODO: put required parameter to simulate the required test condition
+            let { status } = await Finance.getFinanceData();
+
+            //  TODO: insert verifications
+            // ...
+        });
+
+        xtest('404 for a nonexistent company', async () => {
+            // TODO: put required parameter to simulate the required test condition
+            let { status } = await Finance.getFinanceData();
+
+            //  TODO: insert verifications
+            // ...
+        });
+    });
+
+    describe('I have to get values for response headers', () => {
+        test('content-type', async () => {
+            let { headers } = await Finance.getFinanceData();
+            expect(headers['content-type']).toBe('application/json;charset=utf-8');
+        });
+
+        xtest('x-request-id', async () => {
+            let { headers } = await Finance.getFinanceData();
+
+            //  TODO: insert verifications for "x-request-id" header
+            //  You have to use regular expression to verify a headers unique value:
+            //  "d5f838b8-fe84-4f2a-b950-7a07f13264a7"
+            // ...
+
         });
     });
 });
-
