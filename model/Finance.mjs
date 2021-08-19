6d763adbb719e2b7659f468eba345c9318a89b1f
Dmytro-Kanunik <Dmytro.Kanunik@coxautoinc.com>
2021-08-19 21:01:36 +0300

    add linter

--- model/Finance.mjs
+++ model/Finance.mjs
@@ -1,12 +1,12 @@
 import AppConfig from '../configs/AppConfig.cjs';
 
-import axios from "axios";
+import axios from 'axios';
 
 class Finance {
 
     //TODO: this is the bad method that should be replaced by getFinanceData method
     static async getFinanceDataForAAPL() {
-        const params = 'chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance'
+        const params = 'chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance';
         try {
             return await axios.get(AppConfig.baseUrl + params);
         } catch (e) {
@@ -19,7 +19,7 @@ class Finance {
     static async getFinanceData() {
 
         // TODO: the value should contain template variables
-        const params = 'chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance'
+        const params = 'chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance';
         try {
             return await axios.get(AppConfig.baseUrl + params);
         } catch (e) {
